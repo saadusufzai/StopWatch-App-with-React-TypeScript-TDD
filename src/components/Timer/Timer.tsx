228d3b038d3044ab73a0591930b5b4ea43953d56
Saad Aslam <saadusufzai@gmail.com>
2020-09-18 03:29:56 +0500

    Timer Functions not working properly

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,46 +1,63 @@
-import React, {useState} from 'react'
-import Button from '../Button/Button'
-import classes from './Timer.module.css'
+import React, { useState } from "react";
+import Button from "../Button/Button";
+import classes from "./Timer.module.css";
 
-type Timer ={
-  startTimer: ()=>void
-  stopTimer: ()=>void
-  resetTimer: ()=>void
-}
+type Timer = {
+  startTimer: () => any;
+  stopTimer: () => any;
+  resetTimer: () => any;
+};
 
-const Timer = () :JSX.Element =>  {
-    const [minutes, setMinutes] = useState(25)
-    const [seconds, setSeconds] = useState(0)
-    const [isOn, setIsOn] = useState(false)
+const Timer: any = (): JSX.Element => {
+  let [minutes, setMinutes] = useState(25);
+  let [seconds, setSeconds] = useState(0);
+  let [isOn, setIsOn] = useState(false);
+  
 
-  const  startTimer = ():void => {
-        console.log('Starting timer.');
+  const startTimer = () => {
+    if (isOn === true) {
+      return;
+    }
+    const interval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds-=1);
       }
-    
-  const    stopTimer = ()=> {
-        console.log('Stopping timer.');
+     else if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(interval);
+        } else {
+          setMinutes(minutes-=1);
+          setSeconds(59);
+        }
       }
-    
-  const  resetTimer = ()=> {
-        console.log('Resetting timer.');
-      }
+    }, 1000);
+   setIsOn(true)
+  };
 
+  const stopTimer = () => {
+    // clearInterval(interval)
+    clearInterval()
+    setIsOn(false)
+  };
 
+  const resetTimer = () => {
+    console.log("Resetting timer.");
+    setMinutes(25)
+    setSeconds(0)
+  };
 
+  return (
+    <div className={classes.testContainer}>
+      <div className={classes.timeDisplay}>
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </div>
+      <div className={classes.buttonContainer}>
+        <Button btnValue="Start" buttonAction={startTimer} />
+        <Button btnValue="Stop Timer" buttonAction={stopTimer} />
+        <Button btnValue="Reset Timer" buttonAction={resetTimer} />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className={classes.testContainer}>
-            <div className={classes.buttonContainer}>
-            <Button btnValue='Start' buttonAction={startTimer}/>
-            <Button btnValue='Stop Timer' buttonAction={stopTimer}/>
-            <Button btnValue='Reset Timer' buttonAction={resetTimer}/>
-            </div>
-        </div>
-    )
-
-
-
-
-}
-
-export default Timer
+export default Timer;
